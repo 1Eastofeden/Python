e1914513c3a6b4c70a9cace76491e3df19122b79
1Eastofeden <118076910+1Eastofeden@users.noreply.github.com>
2023-05-10 12:13:29 +0300

    Python Dice Game

---
+++ Python Çalışmalarım/Python Dice Game/python.py
@@ -0,0 +1,19 @@
+
+import random
+
+thislist = [1,2,3,4,5,6]
+
+randoms = random.choice(thislist)
+
+if randoms == 1 :
+    print("bad luck", randoms)
+elif randoms == 2 :
+    print("bad luck", randoms)
+elif randoms == 3 :
+    print("little luck", randoms)
+elif randoms == 4 :
+    print("lucky", randoms)
+elif randoms == 5 :
+    print("lucky", randoms)
+elif randoms == 6 :
+    print("lucky", randoms)
