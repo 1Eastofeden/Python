da6771de10959f8c38688a622bf3013a73dd2519
1Eastofeden <118076910+1Eastofeden@users.noreply.github.com>
2023-05-11 19:55:07 +0300

    Python Word Guessing

---
+++ Python Çalışmalarım/Python Word Guessing/python.py
@@ -0,0 +1,11 @@
+
+questionlist = ["hello", "I", "love", "you"]
+
+print(questionlist[0],"", questionlist[1],"","","","","", questionlist[3])
+
+answer = input("complete the sentence hello I ? you :")
+
+if answer == "love" :
+    print("tebrikler cevabınız doğru")
+else :
+    print("üzgünüz cevabınız yanlış")
