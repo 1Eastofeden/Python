be554dd1fb5b5e7965bfb9fdf6a5cb25a528b4c0
muhammed <118076910+1Eastofeden@users.noreply.github.com>
2023-05-20 03:13:14 +0300

    Python Request

---
+++ Python Çalışmalarım/Python Request/puthon.py
@@ -0,0 +1,14 @@
+
+# import requests
+
+# istek = requests.get('https://jsonplaceholder.typicode.com/users/1') # requests.get('') istek oluşturmak için kullanılır  
+
+# if istek.status_code == 200 : # status_code == 200 isteğin başarıyla gönederildiğini kontrol etmek için kullanılır 
+#     a = True 
+#     print(a)
+#     content = istek.text # API'nin içeriğine ulaşmak
+#     print(content)
+# else : 
+#     b = False
+#     print(b)
+
